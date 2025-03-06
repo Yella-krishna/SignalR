7b72b84969557c05ff87c24dbe1405d45805c77d
Ubuntu <ubuntu@agent-test.2dm0hjpxbyeufjyeh0ponoe3td.bx.internal.cloudapp.net>
2025-03-06 06:15:47 +0000

    hello

--- run.cmd
+++ run.cmd
@@ -1,2 +1,3 @@
 @ECHO OFF
 PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0run.ps1' %*; exit $LASTEXITCODE"
+
